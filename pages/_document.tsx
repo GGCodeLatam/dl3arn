84a288be94e050a759c98e22cd4088d9ae7621ae
estebanorlandi4 <estebanorlandi4@gmail.com>
2022-08-29 19:35:53 -0300

    fix: icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -44,6 +44,28 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
 
+          <link
+            rel="apple-touch-icon"
+            sizes="76x76"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0d0b20" />
+          <meta name="msapplication-TileColor" content="#0d0b20" />
+          <meta name="theme-color" content="#0d0b20" />
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
