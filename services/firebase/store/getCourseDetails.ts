db164fba9ca6411caf303e3a2617900f96af9b7b
estebanorlandi4 <estebanorlandi4@gmail.com>
2022-09-01 16:43:43 -0300

    fix: curso sin video rompia la pagina

--- services/firebase/store/getCourseDetails.ts
+++ services/firebase/store/getCourseDetails.ts
@@ -9,46 +9,70 @@ import { getImage } from "../storage";
 import getVideo from "./getVideo";
 
 async function getCourseDetails(id: string): Promise<APIGetCourseById> {
-  const ref = await getDoc(doc(db, "courses", id));
+  try {
+    const ref = await getDoc(doc(db, "courses", id));
 
-  const course = { id: ref.id, ...ref.data() } as CourseModel;
+    const course = { id: ref.id, ...ref.data() } as CourseModel;
 
-  type ParsedSections = Override<
-    Sections,
-    { [key: string]: { position: number; videos: VideoSafeProps[] } }
-  >;
-  const newSections: ParsedSections = {};
-  const videos: (VideoModel & { section: string })[] = [];
+    type ParsedSections = Override<
+      Sections,
+      { [key: string]: { position: number; videos: VideoSafeProps[] } }
+    >;
+    const newSections: ParsedSections = {};
+    const videos: (VideoModel & { section: string })[] = [];
 
-  await Promise.all(
-    Object.entries(course.sections).map(async ([key, value]) => {
-      const sectionVideos = (await Promise.all(
-        value.videos.map(getVideo)
-      )) as VideoModel[];
+    await Promise.all(
+      Object.entries(course.sections).map(async ([key, value]) => {
+        if (!value.videos) return null;
+        const sectionVideos = (await Promise.all(
+          value?.videos.map(getVideo)
+        )) as VideoModel[];
 
-      videos.push(
-        ...sectionVideos.map((video) => ({
-          ...video,
-          section: key,
-        }))
-      );
+        videos.push(
+          ...sectionVideos.map((video) => ({
+            ...video,
+            section: key,
+          }))
+        );
 
-      newSections[key] = {
-        position: value.position,
-        videos: sectionVideos.map((video) => ({
-          id: video.id,
-          name: video.name,
-          free: video.free,
-          duration: "",
-        })),
+        newSections[key] = {
+          position: value.position,
+          videos: sectionVideos.map((video) => ({
+            id: video.id,
+            name: video.name,
+            free: video.free,
+            duration: "",
+          })),
+        };
+        return null;
+      })
+    );
+
+    if (!videos.length) {
+      Object.entries(course.sections).forEach(([section, value]) => {
+        newSections[section] = { position: value?.position || 0, videos: [] };
+      });
+
+      const image = await getImage(course.image);
+      const parsed_course: APIGetCourseById = {
+        ...course,
+        sections: newSections,
       };
-      return null;
-    })
-  );
+      if (!image) return { ...parsed_course, image: "" };
+      return { ...parsed_course, image };
+    }
 
-  if (!videos.length) {
-    Object.entries(course.sections).forEach(([section, value]) => {
-      newSections[section] = { position: value.position, videos: [] };
+    const durations = await getVideosDuration(videos);
+
+    durations.forEach((video) => {
+      newSections[video.section] = {
+        ...newSections[video.section],
+        videos: newSections[video.section].videos.map((current) =>
+          current.id === video.id
+            ? { ...current, duration: video.duration }
+            : current
+        ),
+      };
     });
 
     const image = await getImage(course.image);
@@ -58,28 +82,9 @@ async function getCourseDetails(id: string): Promise<APIGetCourseById> {
     };
     if (!image) return { ...parsed_course, image: "" };
     return { ...parsed_course, image };
+  } catch (e) {
+    return null;
   }
-
-  const durations = await getVideosDuration(videos);
-
-  durations.forEach((video) => {
-    newSections[video.section] = {
-      ...newSections[video.section],
-      videos: newSections[video.section].videos.map((current) =>
-        current.id === video.id
-          ? { ...current, duration: video.duration }
-          : current
-      ),
-    };
-  });
-
-  const image = await getImage(course.image);
-  const parsed_course: APIGetCourseById = {
-    ...course,
-    sections: newSections,
-  };
-  if (!image) return { ...parsed_course, image: "" };
-  return { ...parsed_course, image };
 }
 
 export default getCourseDetails;
